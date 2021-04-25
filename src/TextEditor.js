4a131e02fbe9cf6b26d441b3591648c918be6fb3
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-04-25 19:33:47 +0200

    urls

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState();
 
   useEffect(() => {
-    const s = io("http://localhost:5000/");
+    const s = io("https://googledocsclone.herokuapp.com/");
     setSocket(s);
 
     return () => {
